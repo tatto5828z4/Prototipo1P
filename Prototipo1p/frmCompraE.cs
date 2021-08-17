1eb9b88babfddc551e47883e24994bc4d6b50af0
tatto5828z4 <60116165+tatto5828z4@users.noreply.github.com>
2021-08-17 10:09:14 -0600

    Mantenimiento

---
+++ Prototipo1p/frmCompraE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo1p
+{
+    public partial class frmCompraE : Form
+    {
+        public frmCompraE()
+        {
+            InitializeComponent();
+        }
+    }
+}
